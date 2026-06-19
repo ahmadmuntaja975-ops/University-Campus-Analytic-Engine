c6f38789183758a1d2793fa263502d37032b683d
ahmadmuntajazubair <ahmadmuntaja975@gmail.com>
2026-06-20 03:05:27 +0500

    Update course_ops.cpp
    Implemented Complete Course Management Module

--- course_ops.cpp
+++ course_ops.cpp
@@ -1,5 +1,6 @@
 #include "course_ops.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -20,19 +21,160 @@ void courseMenu()
     switch(choice)
     {
         case 1:
-            addCourse();
+            void addCourse()
+            {
+                string code, name;
+                int creditHours;
+
+                cout << "\nEnter Course Code: ";
+                cin >> code;
+
+                cout << "Enter Course Name: ";
+                cin >> name;
+
+                cout << "Enter Credit Hours: ";
+                cin >> creditHours;
+
+                ofstream file("courses.txt", ios::app);
+
+                file << code << " "
+                    << name << " "
+                    << creditHours << endl;
+
+                file.close();
+
+                cout << "\nCourse Added Successfully!\n";
+            }
             break;
 
         case 2:
-            searchCourse();
+            void searchCourse()
+            {
+                string searchCode;
+                string code, name;
+                int creditHours;
+
+                bool found = false;
+
+                cout << "\nEnter Course Code: ";
+                cin >> searchCode;
+
+                ifstream file("courses.txt");
+
+                while(file >> code >> name >> creditHours)
+                {
+                    if(code == searchCode)
+                    {
+                        cout << "\nCourse Found\n";
+                        cout << "Code: " << code << endl;
+                        cout << "Name: " << name << endl;
+                        cout << "Credit Hours: " << creditHours << endl;
+
+                        found = true;
+                        break;
+                    }
+                }
+
+                file.close();
+
+                if(!found)
+                {
+                    cout << "\nCourse Not Found!\n";
+                }
+}
             break;
 
         case 3:
-            updateCourse();
+            void updateCourse()
+            {
+                string searchCode;
+                string code, name;
+                int creditHours;
+
+                cout << "\nEnter Course Code to Update: ";
+                cin >> searchCode;
+
+                ifstream file("courses.txt");
+                ofstream temp("temp.txt");
+
+                bool found = false;
+
+                while(file >> code >> name >> creditHours)
+                {
+                    if(code == searchCode)
+                    {
+                        found = true;
+
+                        cout << "\nEnter New Course Name: ";
+                        cin >> name;
+
+                        cout << "Enter New Credit Hours: ";
+                        cin >> creditHours;
+
+                        temp << code << " "
+                            << name << " "
+                            << creditHours << endl;
+                    }
+                    else
+                    {
+                        temp << code << " "
+                            << name << " "
+                            << creditHours << endl;
+                    }
+                }
+
+                file.close();
+                temp.close();
+
+                remove("courses.txt");
+                rename("temp.txt", "courses.txt");
+
+                if(found)
+                    cout << "\nCourse Updated Successfully!\n";
+                else
+                    cout << "\nCourse Not Found!\n";
+            }
             break;
 
         case 4:
-            deleteCourse();
+            void deleteCourse()
+            {
+                string searchCode;
+                string code, name;
+                int creditHours;
+
+                cout << "\nEnter Course Code to Delete: ";
+                cin >> searchCode;
+
+                ifstream file("courses.txt");
+                ofstream temp("temp.txt");
+
+                bool found = false;
+
+                while(file >> code >> name >> creditHours)
+                {
+                    if(code == searchCode)
+                    {
+                        found = true;
+                        continue;
+                    }
+
+                    temp << code << " "
+                        << name << " "
+                        << creditHours << endl;
+                }
+
+                file.close();
+                temp.close();
+
+                remove("courses.txt");
+                rename("temp.txt", "courses.txt");
+
+                if(found)
+                    cout << "\nCourse Deleted Successfully!\n";
+                else
+                    cout << "\nCourse Not Found!\n";
+            }
             break;
 
         default:
